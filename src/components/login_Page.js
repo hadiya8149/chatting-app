c6dbc0f943538885476a66673985b209fc34316a
hadiya8149 <hadiya8149@gmail.com>
2023-03-28 21:06:41 +0500

    form validation

--- src/components/login_Page.js
+++ src/components/login_Page.js
@@ -10,39 +10,20 @@ export default function LoginPage(props) {
     const changeAuthMode = () => {
         setAuthMode(authMode === "signin" ? "signup" : "signin");
     }
+    let [email, setEmail]=useState("");
 
-    function handleChange(event){
-        const updatedForm = {...loginForm, [event.target.name]:event.target.value}
-        setLoginForm(updatedForm)
-        // console.log(loginForm)
-    }
-    function isValidEmail(){
-        return /\S+@\S+\.\S+/.test(loginForm.email);
-
-    }
     function handleSubmit(event){
         event.preventDefault()
-        console.log(loginForm)
-
-        console.log(isValidEmail)
-        if (!isValidEmail()){
-            alert("Invalid email")
-
-        }
-        else{
-            console.log("signed in successfully")
-
-        }
     }
 
-    // function handleEmailChange(e){
-    //     setEmail(e);
-    //     console.log(email);
-    // }
+    function handleEmailChange(e){
+        setEmail(e);
+        console.log(email);
+    }
 
-    // function handlePasswordChange(e){
+    function handlePasswordChange(e){
 
-    // }
+    }
     if(authMode === "signin") {
         return (
             <div className='main-content'>
@@ -51,8 +32,8 @@ export default function LoginPage(props) {
                     <form id="login-form" method="post" onSubmit={handleSubmit}>
 
                         <div className="form-group row mb-12">
-                            <input type="text" className="form-control" onChange={handleChange} value={loginForm.email} name="email" id="email" placeholder="Email or username" /></div>
-                        <div className="form-group row mb-12"><input type="password" className="form-control mt-3" onChange={handleChange} value={loginForm.password} name="password" id="password" placeholder="Password" /></div>
+                            <input type="text" className="form-control" value={props.email.value} name="email" id="email" placeholder="Email or username" /></div>
+                        <div className="form-group row mb-12"><input type="text" className="form-control mt-3"  value={props.value.password} name="password" id="password" placeholder="Password" /></div>
                         <div className="form-check mt-4 w-100"><label className="form-check-label text-dark "><input type="checkbox" name="remember" className="form-check-input" />Remember me</label></div>
                         <div className="form-check mt-4 w-100"><span onClick={changeAuthMode} >Create an account</span></div>
 
@@ -76,9 +57,9 @@ else{
                         <div className="form-group row mb-12"><span onClick={changeAuthMode}> Already Registered? Sign in </span></div>
 
                         <div className="form-group row mb-12">
-                            <input type="text" className="form-control" name="email" onChange={handleChange} value={loginForm.email}  id="email" placeholder="Email or username" /></div>
-                        <div className="form-group row mb-12"><input type="text" onChange={handleChange} className="form-control mt-3" value={loginForm.password} name="password" id="password" placeholder="Password" /></div>
-                        <div className="form-group row mb-12"><input type="text" onChange={handleChange} className="form-control mt-3" name="password" id="password" placeholder="Confirm Password" /></div>
+                            <input type="text" className="form-control" name="email" value={props.email.value} onChange={handleEmailChange} id="email" placeholder="Email or username" /></div>
+                        <div className="form-group row mb-12"><input type="text" className="form-control mt-3" value={props.password.value} name="password" id="password" placeholder="Password" /></div>
+                        <div className="form-group row mb-12"><input type="text" className="form-control mt-3" name="password" id="password" placeholder="Confirm Password" /></div>
 
 
                         <div className="form-group row mb-12"><button type="Submit" className="btn btn-primary btn-md my-3 w-100 ">Signup</button></div>
