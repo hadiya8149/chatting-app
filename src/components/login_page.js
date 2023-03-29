a43ac70ee5a26f2456a6267c9691b082539eb4df
hadiya8149 <hadiya8149@gmail.com>
2023-03-29 17:55:15 +0500

    form validation

--- src/components/login_page.js
+++ src/components/login_page.js
@@ -4,6 +4,7 @@ export default function LoginPage(props) {
     const [loginForm, setLoginForm] = useState({
         email:"",
         password:"",
+        password2:""
 
     })
     let[authMode, setAuthMode] = useState("signin");
@@ -20,29 +21,29 @@ export default function LoginPage(props) {
         return /\S+@\S+\.\S+/.test(loginForm.email);
 
     }
+    function isValidPassword(){
+        return /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[^a-zA-Z0-9])(?!.*\s).{8,10}$/.test(loginForm.password);
+    }
     function handleSubmit(event){
         event.preventDefault()
         console.log(loginForm)
 
         console.log(isValidEmail)
         if (!isValidEmail()){
-            alert("Invalid email")
-
-        }
+            alert("Invalid email");
+             var x=document.getElementById("email");
+            x.style.border="2px solid red";     }
         else{
             console.log("signed in successfully")
 
         }
+        if (!isValidPassword()){
+            alert("Password should contain one lowercase , uppercase, numeric digit and a special character.Should be between 8 to 10 characters long")
+            var y = document.getElementById("password")
+            y.style.border="2px solid red"
+        }
     }
 
-    // function handleEmailChange(e){
-    //     setEmail(e);
-    //     console.log(email);
-    // }
-
-    // function handlePasswordChange(e){
-
-    // }
     if(authMode === "signin") {
         return (
             <div className='main-content'>
@@ -72,13 +73,13 @@ else{
             <div className='main-content'>
                 <div className='sides'></div>
                 <div className=' form-panel p-5 d-flex flex-column w-100 '>
-                    <form id="signup-form" method="post">
+                    <form id="signup-form" onSubmit={handleSubmit} method="post">
                         <div className="form-group row mb-12"><span onClick={changeAuthMode}> Already Registered? Sign in </span></div>
 
                         <div className="form-group row mb-12">
                             <input type="text" className="form-control" name="email" onChange={handleChange} value={loginForm.email}  id="email" placeholder="Email or username" /></div>
-                        <div className="form-group row mb-12"><input type="text" onChange={handleChange} className="form-control mt-3" value={loginForm.password} name="password" id="password" placeholder="Password" /></div>
-                        <div className="form-group row mb-12"><input type="text" onChange={handleChange} className="form-control mt-3" name="password" id="password" placeholder="Confirm Password" /></div>
+                        <div className="form-group row mb-12"><input type="password" onChange={handleChange} className="form-control mt-3" value={loginForm.password} name="password" id="password" placeholder="Password" /></div>
+                        <div className="form-group row mb-12"><input type="password" onChange={handleChange} className="form-control mt-3" name="password2" id="password2" placeholder="Confirm Password" /></div>
 
 
                         <div className="form-group row mb-12"><button type="Submit" className="btn btn-primary btn-md my-3 w-100 ">Signup</button></div>
