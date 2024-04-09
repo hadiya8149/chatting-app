85cb2a787879bf2cf3e2f94f93d9a2869274bbe3
hadiya8149 <hadiya8149@gmail.com>
2024-04-09 15:58:43 +0500

    proxy

--- chat/src/setupProxy.js
+++ chat/src/setupProxy.js
@@ -5,14 +5,14 @@ module.exports = function(app) {
  app.use(
     '/api/loginAPI',
     createProxyMiddleware({
-      target: 'http://127.0.0.1:9000/',
+      target: 'http://64.226.79.131:9000/',
       changeOrigin: true,
     })
   );
  app.use(
     '/api/signupAPI',
     createProxyMiddleware({
-      target: 'http://127.0.0.1:9000/',
+      target: 'http://64.226.79.131:9000/',
       changeOrigin: true,
     })
   );
